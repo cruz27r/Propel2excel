c81f5568833a8ad8d9d0e6fcc2722ab516d8fa6f
Rafael Cruz <cruz.ricky.tech@gmail.com>
2023-11-14 23:38:27 -0500

    more changes adding functionality to the navbar

--- propel2excel/src/components/NavBar/navbar.js
+++ propel2excel/src/components/NavBar/navbar.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import logoImage from '../../assets/images/P2E_Logo.png'; // Update the path if necessary
-import './navbar.css'; // Make sure this path is correct
+import logoImage from '../../assets/images/P2E_Logo.png';
+import './navbar.css';
 
 const NavBar = () => {
+  const [showNav, setShowNav] = useState(true);
+  const lastScrollY = useRef(window.scrollY); // Ref to keep track of last scroll position
+  const hideTimer = useRef(); // Ref for the timer ID
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Show nav if scrolling up, hide if scrolling down
+      setShowNav(window.scrollY < lastScrollY.current);
+
+      // Update the last scroll position
+      lastScrollY.current = window.scrollY;
+
+      // Clear any timeout already set
+      clearTimeout(hideTimer.current);
+
+      // If we're not at the top, set a timeout to hide the navbar
+      if (window.scrollY > 0) {
+        hideTimer.current = setTimeout(() => {
+          setShowNav(false);
+        }, 2500); // Hide after 2.5 seconds of inactivity
+      }
+    };
+
+    // Set up the event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(hideTimer.current);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once after the initial render
+
   return (
-    <header className="header">
+    <header className={`header ${showNav ? 'active' : 'hidden'}`}>
       <Link to="/">
         <img src={logoImage} alt="Company Logo" className="logo" />
       </Link>
       <nav className="nav-links">
-        {/*<Link to="/home">Home</Link>*/}
         <Link to="/events-for-readiness">Events for Readiness</Link>
         <Link to="/submit-linkedin">Submit LinkedIn</Link>
         <Link to="/submit-resume">Submit Resume</Link>
