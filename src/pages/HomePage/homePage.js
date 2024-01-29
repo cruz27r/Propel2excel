065bac834a33f02a1fe31ad331a431cb408a7826
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-01-29 00:22:29 -0500

    major chnages and additions

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -5,7 +5,8 @@ import './homePage.css';
 import photo1 from '../../assets/images/company-office-headshots-hartford-connecticut.jpg';
 import photo2 from '../../assets/images/networking_large_table.jpg';
 import photo3 from '../../assets/images/networking_table.jpg';
-import background from '../../assets/images/company-office-headshots-hartford-connecticut.jpg';
+import logoImage from '../../assets/images/Full-blue-p2e-logoAsset 6.png';
+import background from '../../assets/images/fellow-collage2.jpg';
 import video1 from '../../assets/videos/test_networking_vid.mp4';
 import workshopImage from '../../assets/images/looking_over_code.jpg';
 import networkingImage from '../../assets/images/looking_over_code.jpg';
@@ -25,7 +26,9 @@ function Home() {
     <div className="App">
       <main className='banner' style={{ backgroundImage: `url(${background})` }}>
         <div className="banner-content">
-          <h1 className="main-title">Propel2Excel</h1>
+          <h1 className="main-title">
+            <img src={logoImage} alt="Propel2Excel Logo" />
+          </h1>
           <p className="main-description">Bridging the Ivey-league recruiting experience to ambitious students at non-target universities</p>
           <Link to="/become-p2e" className="details-button">P2E cohort</Link>
         </div>
@@ -35,62 +38,68 @@ function Home() {
         <p className="p2e-text">Propel2Excel is dedicated to empowering underrepresented students in business, providing a bridge to prestigious career opportunities through comprehensive coaching and networking. Our mission is to bridge the gap between non-target universities and Fortune 100 companies, offering a transformative experience that opens doors to internships and lucrative careers. With a focus on inclusivity and excellence, we aim to create a diverse pipeline of future business leaders.</p>
       </div>
 
+      {/* Corporate Logos Section */}
       <section className="corporate-logos">
         <img src={photo2} alt="Networking at a large table" />
         <div className="text-box">
-          <h2>Empowering Partnerships with Corporate Giants</h2>
+          <h2>Discover Our Corporate Network</h2>
           <p>At Propel2Excel, we have established strong partnerships with industry giants such as Google, Microsoft, and Amazon. These collaborations create unique opportunities for our participants, from internships to job placements. Our corporate network offers invaluable insights and mentorship from experienced professionals in these renowned companies.</p>
           <Link to="/corporate-companies" className="details-button">Discover Our Corporate Network</Link>
         </div>
       </section>
 
+      {/* Mentorship Section */}
       <section className="mentors-section">
         <img src={photo3} alt="Networking around a table" />
         <div className="text-box">
-          <h2>Mentorship with a Personal Touch</h2>
+          <h2>Meet Our Mentors</h2>
           <p>Our mentorship program is the heart of Propel2Excel. Experienced mentors from leading firms offer weekly coaching, guiding students in their career journeys. Beyond group coaching, students can request private 1-on-1 meetings with mentors for personalized advice. Our mentors play a crucial role in bringing students to 80% of speaking engagements and ensuring progress in LinkedIn and resume rebranding.</p>
           <Link to="/mentors" className="details-button">Meet Our Mentors</Link>
         </div>
       </section>
 
+      {/* Career Coach Section */}
       <section className="coach-section">
         <div className="text-box">
-          <h2>Tailored Coaching for Career Excellence</h2>
+          <h2>Connect with a Career Coach</h2>
           <p>Our 1-on-1 coaching program focuses on specific career fields, offering interview preparation, LinkedIn rebranding, and more. Our coaches are dedicated to helping students excel in their chosen paths. Additionally, students gain insights and introductions to senior leadership within their respective companies, paving the way for successful careers.</p>
           <Link to="/coach" className="details-button">Connect with a Career Coach</Link>
         </div>
         <video src={video1} autoPlay muted loop playsInline />
       </section>
 
-      <section className="events-section">
-        <div className="events-content">
+      {/* Events Section */}
+      <section className="events-services-section">
+        <div className="events-services-content">
           <h2>Events That Lead and Inspire</h2>
           <p>Our exclusive events provide invaluable insights into industry practices and networking opportunities, preparing our fellows to become industry leaders.</p>
-          <ul className="events-bullets">
+          <ul className="events-services-bullets">
             <li onMouseOver={() => handleHoverEvent('workshop')} className={activeEvent === 'workshop' ? 'active' : ''}>Interactive Workshops</li>
             <li onMouseOver={() => handleHoverEvent('networking')} className={activeEvent === 'networking' ? 'active' : ''}>Networking Events</li>
             <li onMouseOver={() => handleHoverEvent('speaker')} className={activeEvent === 'speaker' ? 'active' : ''}>Speaker Series</li>
           </ul>
         </div>
-        <div className="events-image">
+        <div className="events-services-image">
           <img id="events-img" src={workshopImage} alt="Event" />
         </div>
       </section>
 
-      <section className="services-section">
-        <div className="services-content">
+      {/* Services Section */}
+      <section className="events-services-section">
+        <div className="events-services-content">
           <h2>Comprehensive Services for Career Advancement</h2>
           <p>We offer a range of services designed to equip students with the necessary tools for career success, from LinkedIn rebranding to expert interview preparation.</p>
-          <ul className="services-bullets">
+          <ul className="events-services-bullets">
             <li onMouseOver={() => handleHoverEvent('coaching')} className={activeEvent === 'coaching' ? 'active' : ''}>Career Coaching</li>
             <li onMouseOver={() => handleHoverEvent('resume')} className={activeEvent === 'resume' ? 'active' : ''}>Resume Workshops</li>
             <li onMouseOver={() => handleHoverEvent('interview')} className={activeEvent === 'interview' ? 'active' : ''}>Interview Preparation</li>
           </ul>
         </div>
-        <div className="services-image">
+        <div className="events-services-image">
           <img id="services-img" src={careerCoachingImage} alt="Service" />
         </div>
       </section>
+
     </div>
   );
 }
--- src/pages/UniversityPartners/UniversityPartners.js
+++ src/pages/UniversityPartners/UniversityPartners.js
@@ -1,44 +1,71 @@
 import React from 'react';
 import './UniversityPartners.css';
 import universityPartnerLogo from '../../assets/images/P2E_Logo.png';
+import universityPartnerLogo1 from '../../assets/images/umBoston.jpg';
+import universityPartnerLogo2 from '../../assets/images/umassA.jpg';
+import universityPartnerLogo3 from '../../assets/images/bc.png';
+import universityPartnerLogo4 from '../../assets/images/Northeastern.png';
+import universityPartnerLogo5 from '../../assets/images/Wentworth.png';
+import universityPartnerLogo6 from '../../assets/images/P2E_Logo.png';
+import universityPartnerLogo7 from '../../assets/images/P2E_Logo.png';
 
 function UniversityPartners() {
-  const universityPartners = [
+  const reachedUniversities = [
     {
       name: 'University of Massachusetts Boston',
-      description: 'A current partner providing educational resources and support.',
-      image: universityPartnerLogo
+      description: 'A university we have reached and are currently partnering with, providing educational resources and support.',
+      image: universityPartnerLogo1
     },
     {
-      name: 'University Partner 1',
-      description: 'A renowned university with a strong research focus.',
-      image: universityPartnerLogo
+      name: 'Reached University 1',
+      description: 'Another university we have successfully partnered with, known for its strong research focus.',
+      image: universityPartnerLogo2
     },
-    // Add more university partners as needed...
+    {
+      name: 'Upcoming University 2',
+      description: 'Another institution on our radar, renowned for its international studies programs.',
+      image: universityPartnerLogo3
+    },
+    {
+      name: 'Upcoming University 2',
+      description: 'Another institution on our radar, renowned for its international studies programs.',
+      image: universityPartnerLogo4
+    },
+    // Add more reached universities as needed...
   ];
 
-  const anticipatedPartners = [
+  const upcomingUniversities = [
     {
-      name: 'Future University Partner 1',
-      description: 'A prospective partner with a focus on innovation in education.',
-      image: universityPartnerLogo
+      name: 'Upcoming University 1',
+      description: 'A prospective university we are planning to partner with, known for its focus on innovation in education.',
+      image: universityPartnerLogo5
     },
     {
-      name: 'Future University Partner 2',
-      description: 'A leading institution in international studies.',
-      image: universityPartnerLogo
+      name: 'Upcoming University 2',
+      description: 'Another institution on our radar, renowned for its international studies programs.',
+      image: universityPartnerLogo3
     },
-    // Add more anticipated university partners as needed...
+    {
+      name: 'Upcoming University 2',
+      description: 'Another institution on our radar, renowned for its international studies programs.',
+      image: universityPartnerLogo2
+    },
+    {
+      name: 'Upcoming University 2',
+      description: 'Another institution on our radar, renowned for its international studies programs.',
+      image: universityPartnerLogo1
+    },
+    // Add more upcoming universities as needed...
   ];
 
   return (
     <div className="partners-container">
-      <h1>Our University Partners</h1>
+      <h1>Our Reached Universities</h1>
       
-      <section className="current-partners">
-        <h2>Current University Represented</h2>
+      <section className="reached-universities">
+        <h2>Universities We Have Successfully Partnered With</h2>
         <div className="partners-grid">
-          {universityPartners.map((partner, index) => (
+          {reachedUniversities.map((partner, index) => (
             <div key={index} className="partner">
               <img src={partner.image} alt={partner.name} className="partner-logo" />
               <h3>{partner.name}</h3>
@@ -48,10 +75,10 @@ function UniversityPartners() {
         </div>
       </section>
 
-      <section className="anticipated-partners">
-        <h2>Anticipated University Partners</h2>
+      <section className="upcoming-universities">
+        <h2>Universities We're Coming to Soon</h2>
         <div className="partners-grid">
-          {anticipatedPartners.map((partner, index) => (
+          {upcomingUniversities.map((partner, index) => (
             <div key={index} className="partner">
               <img src={partner.image} alt={partner.name} className="partner-logo" />
               <h3>{partner.name}</h3>
