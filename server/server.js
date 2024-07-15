e0530684895f95d53fce065e8498143b9fdbd8c7
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-07-15 12:12:37 -0400

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const formRoutes = require('./routes/formRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -26,6 +30,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use form routes
 app.use('/api/forms', formRoutes);
+
+// Catch-all route to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send('Sorry, cannot find that!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
