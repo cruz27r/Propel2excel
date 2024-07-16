66ce440481aaecb33b59c5c9cc7111928ad3b9b9
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-07-16 01:26:35 -0400

    server chnages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 require('dotenv').config(); // Load environment variables
 
+const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors()); // Enable CORS
 
 // MySQL Connection
 const db = mysql.createConnection({
@@ -18,8 +27,24 @@ db.connect(err => {
     console.log('Connected to MySQL');
 });
 
-// Example query to test connection
-db.query('SELECT 1', (err, results) => {
-    if (err) throw err;
-    console.log(results);
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Example route with database query
+app.get('/data', (req, res) => {
+    const query = 'SELECT * FROM your_table';
+    db.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json(results);
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
