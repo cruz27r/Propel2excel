44e2377220f93f06fdf596012661a9077274bf55
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-07-18 16:59:04 -0400

    server js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mysql2 = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 
@@ -27,6 +29,39 @@ db.connect(err => {
     console.log('Connected to MySQL');
 });
 
+// Set up storage engine
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Initialize upload
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 }, // 1MB file size limit
+    fileFilter: function(req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('myFile');
+
+// Check File Type
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif|txt/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
 // Define a simple route
 app.get('/', (req, res) => {
     res.send('Hello, world!');
@@ -40,6 +75,23 @@ app.get('/data', (req, res) => {
             return res.status(500).send(err);
         }
         res.json(results);
+    });
+});
+
+// File upload route
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            if (req.file == undefined) {
+                res.status(400).send('Error: No File Selected!');
+            } else {
+                res.json({
+                    file: `uploads/${req.file.filename}`
+                });
+            }
+        }
     });
 });
 
