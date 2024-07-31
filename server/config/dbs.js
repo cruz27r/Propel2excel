a755d4b4c588ab89f3a02795589c3d642a94d151
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-07-31 12:51:31 -0400

    tests to make sql server connect

--- server/config/dbs.js
+++ server/config/dbs.js
@@ -1,8 +1,8 @@
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_USER:', process.env.DB_USER);
+// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+// console.log('DB_NAME:', process.env.DB_NAME);
+// console.log('DB_PORT:', process.env.DB_PORT);
 
 const mysql2 = require('mysql2');
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 require('dotenv').config(); // Load environment variables
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
