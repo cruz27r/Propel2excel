c88161f3dd774dc81a2cf9ea04d3e08122392a37
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-09-16 08:57:19 -0400

    attempting to override the navbar issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar.js';
+import Navbar from './components/NavBar/navbar.js';
 import Homepage from './pages/Homepage.js';
 import MainApplication from './pages/MainApplication/MainApplication';
 import HiringBoard from './components/HiringBoard/HiringBoard';
